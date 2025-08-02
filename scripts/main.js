dae3b8ce84ff7273b92d28d49646d5f7f7108eb7
Pedro Robalo <pedro.robalo@uphold.com>
2025-08-02 23:06:26 +0100

    Testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+console.log("Hello world");
+console.log("This is a test message for the console.");
